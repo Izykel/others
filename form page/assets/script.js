88c6a03939fefa135aeb0fd56c0e24bb91990f56
Izykel <124392480+Izykel@users.noreply.github.com>
2025-12-30 10:51:30 +0100

    Add files via upload

---
+++ form page/assets/script.js
@@ -0,0 +1,13 @@
+function collectinput(){
+    let fullName = document.getElementById("fname");
+    let fullNameValue = fullName.value ;
+    console.log(fullNameValue);
+
+    let email = document.getElementById("email");
+    let emailValue = email.value ;
+    console.log(emailValue);
+}
+
+
+
+
